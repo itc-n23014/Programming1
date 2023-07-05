db1ba582f93267b2503bf2f8a5aeea14c456669d
Komine Mitsuhiko <n23014@std.it-college.ac.jp>
2023-07-05 08:49:16 +0000

    2-7 追加

---
+++ CHAPTER02/Q2_7_1.py
@@ -0,0 +1,6 @@
+my_list = []
+b = "orange apple grape banana".split()
+for i in b:
+    my_list.append(i)
+
+print(my_list)
---
+++ CHAPTER02/Q2_7_2.py
@@ -0,0 +1,9 @@
+my_list = ["orange", "apple", "grape", "banana"]
+
+# 昇順
+my_list.sort()
+print(my_list)
+
+# 降順
+my_list.reverse()
+print(my_list)
