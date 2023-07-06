aa8caa07d192d63274942cc8d9bf70f28452b084
Komine Mitsuhiko <n23014@std.it-college.ac.jp>
2023-07-06 07:02:42 +0000

    2-7-2 編集

--- CHAPTER02/Q2_7_2.py
+++ CHAPTER02/Q2_7_2.py
@@ -1,9 +1,5 @@
 my_list = ["orange", "apple", "grape", "banana"]
-
 # 昇順
-my_list.sort()
-print(my_list)
-
+print(sorted(my_list))
 # 降順
-my_list.reverse()
-print(my_list)
+print(sorted(my_list)[::-1])
