72bd3156f2d752296680ab40708562d0ad883191
Komine Mitsuhiko <n23014@std.it-college.ac.jp>
2023-07-11 07:51:15 +0000

    2-13 追加

---
+++ CHAPTER02/Q2_13_1.py
@@ -0,0 +1,5 @@
+from math import pow
+
+# 出力結果がfloat型であることから、
+# 組み込み関数ではなくモジュールが使われたことがわかる
+print(pow(2, 3))
---
+++ CHAPTER02/Q2_13_2.py
@@ -0,0 +1,6 @@
+import random
+from random import randint
+
+# どちらのモジュールも正しく読み込まれているので、正常に動作する
+print(random.randint(0, 10))
+print(randint(0, 10))
