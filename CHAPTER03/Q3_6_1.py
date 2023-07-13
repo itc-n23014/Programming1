a35dd2bc8e36bbe5312bb2a38cf9a148084d1d26
Komine Mitsuhiko <n23014@std.it-college.ac.jp>
2023-07-13 07:37:27 +0000

    3-6 追加

---
+++ CHAPTER03/Q3_6_1.py
@@ -0,0 +1,8 @@
+from random import sample
+
+str4 = "".join(sample([str(i) for i in range(10)], k=4))
+a = 0
+while a == 0:
+    num4 = input()
+    a = 1 if num4 == str4 else 0
+    print("OK") if a == 1 else print(f"{num4} NG")
