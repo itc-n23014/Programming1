ac57aaf0b5829cc662a430c3edbfa56092c8c277
Komine Mitsuhiko <n23014@std.it-college.ac.jp>
2023-07-19 07:32:30 +0000

    3-6-2 修正

--- CHAPTER03/Q3_6_2.py
+++ CHAPTER03/Q3_6_2.py
@@ -3,7 +3,7 @@ from random import sample
 num = sample([str(i) for i in range(10)], k=4)
 
 while True:
-    val = [i for i in input("input 4 numbers:")]
+    val = [i for i in input("input 4 numbers: ")]
     list = [val[i] if val[i] == num[i] else "X" for i in range(4)]
     print("-> " + "".join(list))
     if val == num:
