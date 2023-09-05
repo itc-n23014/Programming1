4bd41f1eb3f733194cbd84688033b08c99abb676
Komine Mitsuhiko <n23014@std.it-college.ac.jp>
2023-09-05 12:47:05 +0000

    6-5-2 修正

--- CHAPTER06/6-5/Q6_5_2.py
+++ CHAPTER06/6-5/Q6_5_2.py
@@ -2,6 +2,8 @@ import my_math2
 
 x, y, exp = 2, 5, 32
 ans = my_math2.my_pow(x, y)
-print("Test my_pow({},{}) -> {}, exp: {} ---- ".format(x, y, ans, exp), end="")
+print(f"Test my_pow({x},{y}) -> {ans}, exp: {exp} ---- ", end="")
 if ans == exp:
     print("Test OK")
+else:
+    print("Test Fail")
